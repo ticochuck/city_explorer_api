18be2c463f8e681a5a986ba383fcb6c2f14bd402
ticochuck <chucklivilla@gmail.com>
2020-04-09 21:56:29 -0700

    completed locations

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 'use strict';
 
-console.log('hola from the server');
-
 require('dotenv').config();
 
 const cors = require('cors');
 const express = require('express');
-
 const PORT = process.env.PORT;
-
 const app = express();
+
 app.use(cors());
 
 app.get( '/test', (request, response) => {
   const name = request.query.name;
-  response.send( `Hello. ${name}`);
+  response.send(`Hello ${name}`);
 });
 
-app.listen( PORT, () => console.log('server is alive on ', PORT));
+app.get('/location', handleLocation);
 
+function handleLocation( request, response ) {
+  let city = request.query.city;
+  let locationData = require('./data/geo.json');
+  let location = new Location(city, locationData[0]);
+  response.json(location);
+}
 
+function Location(city, data) {
+  this.search_query = city;
+  this.formatted_query = data.display_name;
+  this.latitude = data.lat;
+  this.longitude = data.lon;
+}
+
+app.listen( PORT, () => console.log('Server is up on ', PORT));
